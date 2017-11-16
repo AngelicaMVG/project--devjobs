e3b162285495264600fd96942cda8bc0805c89d9
Angelica Velazquez <angie.velazquez2211@gmail.com>
2017-11-16 13:49:29 -0600

    committing work from part-02

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ const fs = require("fs-extra");
 
 const app = express();
 
-const PATH = `${__dirname}/src/views/home.html`;
-app.use("/", (req, res) => {
-  fs.readFile(PATH, "utf8").then(data => {
-    res.send(data);
-  });
+const pageRouter = require("./src/routers/pageRouter.js");
+const apiRouter = require("./src/routers/apiRouter.js");
+
+app.use("/", pageRouter);
+app.use("/api", apiRouter);
+app.use(express.static(__dirname + "/public"));
+
+app.use((req, res) => {
+  res.send("<h1>404. Not found.</h1>");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`app lisening on port ${PORT}`);
+  console.log(`APP LISTENING ON PORT ${PORT}`);
 });
